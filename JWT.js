35e3fa8e6b5e6a96a04eb6a5257504414178431e
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-17 22:20:06 +0500

    Update JWT.js

--- JWT.js
+++ JWT.js
@@ -1,15 +1,23 @@
 const express = require('express');
+
 const ejs = require('ejs');
+
 const path = require('path');
+
 const bodyParser = require('body-parser');
+
 const app = express();
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
+
 app.set('views', path.resolve('./views'));
 
 app.get('/', (req, res) => {
     res.render('Url_Generate');
+    
 });
 
 app.post('/generate', (req, res) => {
@@ -19,7 +27,7 @@ app.post('/generate', (req, res) => {
 
 app.get('/redirect/:url', (req, res) => {
     const url = req.params.url;
-    const fullURL = 'https://' + url; // simple add https
+    const fullURL = 'https://' + url; 
     res.redirect(fullURL);
 });
 
@@ -27,3 +35,4 @@ const port = 4200;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
